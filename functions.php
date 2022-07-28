29937e08e24d0a09dbcc48595c08e2d0e181c82f
Phumlani Mhlungu <74601957+mhlunguep@users.noreply.github.com>
2022-07-28 19:52:58 +0200

    update

--- functions.php
+++ functions.php
@@ -6,8 +6,10 @@
 
 // Includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
+include(get_theme_file_path('/includes/front/head.php'));
 
 // Hooks
 
-add_action( 'wp_enqueue_scripts', 'u_enqueue_style' );
-add_action( 'wp_enqueue_scripts', 'u_enqueue_script' );
+add_action('wp_enqueue_scripts', 'u_enqueue_style');
+add_action('wp_enqueue_scripts', 'u_enqueue_script');
+add_action('wp_head', 'u_head', 5);
